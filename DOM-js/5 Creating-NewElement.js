07a9121e9dee3b396e28008938b83e00afe04972
Tamil32 <tamiselvan1432@gmail.com>
2025-01-27 00:40:24 +0530

    newfile

---
+++ DOM-js/5 Creating-NewElement.js
@@ -0,0 +1,32 @@
+// Crating a new elements
+
+/* <li class="collection-item">
+List Item 5
+<a href="#" class="delete-item secondary-content">   Reference for creating a new element
+  <i class="fa fa-remove"></i>
+</a>
+</li> */
+
+
+// create a new li element
+
+const li = document.createElement('li');
+li.className ="collection-item";
+li.id ="item-6";
+li.innerText = "TAMIL-6";
+
+// create a new link element
+
+const link = document.createElement('a');
+link.className = "delete-item secondary-content";
+link.href = "#";
+link.innerHTML = `<i class="fa fa-remove"></i>`;
+
+// append link into li
+
+li.appendChild(link);
+
+
+// append li into ul
+
+document.querySelector('ul.collection').appendChild(li);
